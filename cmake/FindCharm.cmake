918f085c9e0626e4b2dd9b4c423af5994985482c
Bryan Lunt <bjlunt2@illinois.edu>
2018-12-10 19:29:02 -0600

    slight alteration

--- cmake/FindCharm.cmake
+++ cmake/FindCharm.cmake
@@ -18,16 +18,18 @@ mark_as_advanced(CHARM_COMPILER)
 
 #Get the version
 if(CHARM_COMPILER)
-execute_process(COMMAND ${CHARM_COMPILER} -V
+	find_package(MPI)
+
+	execute_process(COMMAND ${CHARM_COMPILER} -V
 				OUTPUT_VARIABLE charmc_version
 				ERROR_QUIET
 				OUTPUT_STRIP_TRAILING_WHITESPACE
 			)
-if(charmc_version MATCHES "^Charm\\+\\+ Version [0-9.]+")
-	string(REGEX REPLACE "Charm\\+\\+ Version ([0-9.]+).*" "\\1" CHARM_VERSION_STRING "${charmc_version}")
-endif()
+	if(charmc_version MATCHES "^Charm\\+\\+ Version [0-9.]+")
+		string(REGEX REPLACE "Charm\\+\\+ Version ([0-9.]+).*" "\\1" CHARM_VERSION_STRING "${charmc_version}")
+	endif()
 	unset(charmc_version)
-endif()
+endif(CHARM_COMPILER)
 
 #CHARMXI charm module compiler
 find_program(CHARMXI_COMPILER
@@ -41,30 +43,48 @@ mark_as_advanced(CHARMXI_COMPILER)
 
 #Get all options linking, etc.
 if(CHARM_COMPILER)
-execute_process(COMMAND ${CHARM_COMPILER} -print-building-blocks
+	execute_process(COMMAND ${CHARM_COMPILER} -print-building-blocks
 				OUTPUT_VARIABLE charmc_all_variables
 				ERROR_QUIET
 				OUTPUT_STRIP_TRAILING_WHITESPACE
 			)
-endif()
+
+
+	string(REPLACE "\n" ";" charmc_all_variables_list ${charmc_all_variables})
+	#TODO: loop and find all such variables, not hardcoded
+	##if(charmc_all_variables MATCHES "CHARM_CC_FLAGS='.*'")
+	##	message(FATAL_ERROR "Found charm CC flags " ${charmc_all_variables})
+	##	##string(REGEX REPLACE "Charm\\+\\+ Version ([0-9.]+).*" "\\1" CHARM_VERSION_STRING "${charmc_version}")
+	##endif()
+	foreach(one_charm_variable_line ${charmc_all_variables_list})
+		string(REGEX REPLACE "^(.*)='(.*)'$" "\\1" ONE_CHARM_VAR_NAME ${one_charm_variable_line})
+		string(REGEX REPLACE "^(.*)='(.*)'$" "\\2" ONE_CHARM_VAR_VALUE ${one_charm_variable_line})
+		#message("BALAHA\n" ${ONE_CHARM_VAR_NAME} " IS EQUAL TO " ${ONE_CHARM_VAR_VALUE})
+		set(${ONE_CHARM_VAR_NAME} ${ONE_CHARM_VAR_VALUE})
+	endforeach()
+	unset(charmc_all_variables_list)
+	unset(charmc_all_variables)
+	#link_directories(${target_name} ${CHARMLIB} ${CHARMLIBSO}) #Don't like that this will make everyting have that globally.
+endif(CHARM_COMPILER)
 
 if(CHARMXI_COMPILER)
+	define_property(TARGET PROPERTY "CHARM_SOURCES"
+		BRIEF_DOCS "Sources for charmxi"
+		FULL_DOCS  "List of source files that the charm module compiler should interpret."
+	)
 
 	function(set_charm_target target_name)
-		set(options )
-		set(oneValueArgs )
-		set(multiValueArgs CHARM_SOURCES ADDITIONAL_CHARM_SOURCES CHARM_MODULES )
+		set(options SEARCH STANDALONE NOMAIN) #Tells if we want to search for .ci files in the basic sources list
+		set(oneValueArgs TRACEMODE) #TODO: actually look at charmc to figure out how to properly build traces
+		set(multiValueArgs CHARM_SOURCES CHARM_MODULES )
 		cmake_parse_arguments(SET_CHARM_TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-	#	define_property(TARGET PROPERTY "CHARM_SOURCES"
-	#	BRIEF_DOCS "Sources for charmxi"
-	#	FULL_DOCS  "List of source files that the charm module compiler should interpret."
-	#)
+
 		#examine all the sources and find any charm sources.
 		#print_target_properties(${target_name})
 
 		get_target_property(ALL_SOURCES_PATHS ${target_name} SOURCES)
 
-		set(TMP_CHARM_SOURCES )
+		set(TMP_CHARM_SOURCES ) #TODO: Start with / append any charm sources provided here.
 		set(TMP_NON_CHARM_SOURCES )
 
 		foreach(one_source ${ALL_SOURCES_PATHS})
@@ -87,11 +107,15 @@ if(CHARMXI_COMPILER)
 		#message("all charm sources : " "${TMP_CHARM_SOURCES}")
 		#message("all non-charm sources : " "${TMP_NON_CHARM_SOURCES}")
 
-		message("THE CHARMXI COMPILER IS " ${CHARMXI_COMPILER})
+		#message("THE CHARMXI COMPILER IS " ${CHARMXI_COMPILER})
 
 		foreach(one_charm_source ${TMP_CHARM_SOURCES})
 			get_filename_component(SINGLE_CHARM_DEFAULT_OUTPUT ${one_charm_source} NAME)
 			string(REGEX REPLACE "\\.ci$" "" SINGLE_CHARM_DEFAULT_OUTPUT ${SINGLE_CHARM_DEFAULT_OUTPUT})
+
+			#TODO: We should create a directory that these generated files go into.
+			#If only certain modules were asked for, we should generate those into a non-default directory.
+
 			list(APPEND TMP_NON_CHARM_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/${SINGLE_CHARM_DEFAULT_OUTPUT}.decl.h")
 			list(APPEND TMP_NON_CHARM_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/${SINGLE_CHARM_DEFAULT_OUTPUT}.def.h")
 			include_directories(${target_name} ${CMAKE_CURRENT_BINARY_DIR})
@@ -99,7 +123,6 @@ if(CHARMXI_COMPILER)
 			#If we use an OUTPUT type custom_command, and alter the target's sources list, we might avoid that.
 			#message("one_charm_source : " ${CMAKE_CURRENT_SOURCE_DIR}/${one_charm_source} )
 			set(SET_CHARM_TARGET_SINGLE_CHARM_SOURCE_FULL_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${one_charm_source})
-			#add_custom_command(TARGET ${target_name}
 			add_custom_command(
 				PRE_BUILD
 				OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${SINGLE_CHARM_DEFAULT_OUTPUT}.decl.h ${CMAKE_CURRENT_BINARY_DIR}/${SINGLE_CHARM_DEFAULT_OUTPUT}.def.h
@@ -108,12 +131,36 @@ if(CHARMXI_COMPILER)
 				DEPENDS ${SET_CHARM_TARGET_SINGLE_CHARM_SOURCE_FULL_PATH}
 				VERBATIM
 			)
+
+			#TODO: needs to be per module
+			set(mod_init_src "${CMAKE_CURRENT_BINARY_DIR}/${SINGLE_CHARM_DEFAULT_OUTPUT}_modinit.C")
+			list(APPEND TMP_NON_CHARM_SOURCES ${mod_init_src})
+			add_custom_command(
+				PRE_BUILD
+				OUTPUT ${mod_init_src}
+				COMMAND echo "void _registerExternalModules(char **argv) { (void)argv; } void _createTraces(char **argv) {(void)argv;}" >> ${mod_init_src}
+				WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+				VERBATIM
+			)
+
+
+			#todo: each module needs a modInit
 		endforeach()
 
 		set_target_properties(${target_name} PROPERTIES SOURCES "${TMP_NON_CHARM_SOURCES}" SCOPE PARENT_SCOPE)
 		#TODO: append to if the charm sources property already exists
 		set_target_properties(${target_name} PROPERTIES "CHARM_SOURCES" "${TMP_CHARM_SOURCES}" SCOPE PARENT_SCOPE)
 
+		#compile and linking flags
+		#TODO: Charm can be built without MPI, can't it?
+		#TODO: Detect the language C/CXX etc.
+		#TODO: Get the last compiler/linker flags dynamically from interrogating charmc, not hardcoded as they are here "-m64 etc."
+		include_directories(${target_name} ${MPI_CXX_INCLUDE_PATH})
+		target_link_libraries(${target_name} ${MPI_CXX_LIBRARIES})
+		set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS "${CHARM_CXX_FLAGS} ${MPI_CXX_COMPILE_FLAGS} -m64 -fPIC " SCOPE PARENT_SCOPE)
+		set_target_properties(${target_name} PROPERTIES LINK_FLAGS "${CHARM_LDXX_FLAGS} ${MPI_CXX_LINK_FLAGS} -m64 -fPIC -rdynamic " SCOPE PARENT_SCOPE)
+
+		include_directories(${target_name} ${CHARMINC})
 
 	endfunction()
 endif(CHARMXI_COMPILER)
